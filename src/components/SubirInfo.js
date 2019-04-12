744f1b1c095fbbad50f1625429e57bc2a1676ae7
Mefistófeles Mireles <mefistofelesmireles@MacBook-Air-de-Mefistofeles.local>
2019-04-11 21:52:55 -0500

    final

--- src/components/SubirInfo.js
+++ src/components/SubirInfo.js
@@ -98,7 +98,7 @@ class SubirInfo extends Component {
                 <input onChange={this.onInputChange} type="text" className="form-control" id="cp" placeholder="" />
               </div>
               <div className= "boton">
-              <Link to = "https://heybuddyfinal.herokuapp.com" >
+              <Link to = "/" >
               <button className="btn btn-success" type="submit" >
               Continuar
               </button>
