f289904ffb9458ede7a55d902a9320e48c1d8eb8
Mefistófeles Mireles <mefistofelesmireles@MacBook-Air-de-Mefistofeles.local>
2019-04-11 21:10:51 -0500

    Filtro busqueda

--- src/components/Buscar.js
+++ src/components/Buscar.js
@@ -1,12 +1,61 @@
 import React, { Component } from 'react';
 import Cardpersona from './Cardpersona';
 import Persona from './Persona';
+import axios from 'axios'
 
 class Buscar extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      resultados: [], 
+      query: '',
+      ejecuto: false
+    }
   }
+
+  filtrar = (e) => {
+    const { id, value } = e.target;
+    console.log(id, value);
+      this.setState()
+    this.setState({ [id]: value })
+  }
+
+  buscar = (event) => {
+    event.preventDefault();
+    this.setState({ejecuto: true})
+    axios.get('https://heybuddydevf.herokuapp.com/buscar?tipo=' + this.state.query)
+      .then(res => {
+        console.log(res.data)
+        this.setState({ resultados: res.data });
+      }).catch(err => {
+        console.log('Hubo un error', err)
+      })
+  }
+
+  renderizarResultados = () => {
+    if (this.state.resultados.length > 0) {
+      return (
+        this.state.resultados.map(r => {
+          return (
+            <div className="col-12 col-sm-6 col-md-3">
+              <Cardpersona personas={r} />
+            </div>
+          )
+        })
+      )
+    } else {
+      return (
+        <div>
+          {
+            (this.state.query.length > 0 && this.state.ejecuto)
+            ? <p>Sin resultados</p>
+            : <Persona />
+          }
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div className="Buscar">
@@ -14,27 +63,34 @@ class Buscar extends Component {
           <a className="navbar-brand" href="#">HeyBuddy!
               </a>
         </nav>
-        <div className= "container">
-        <div className= "row">
-        <div className= "col-sm">
-        <div className = "Input">
-        <div className="input-group mb-3">
-          <input type="text" className="form-control" placeholder="Ingresa busqueda" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-            <div className="input-group-append">
-              <button className="btn btn-success" type="button" id="button-addon2">Buscar</button>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">
+             <form onSubmit={this.buscar}>
+              <div className="Input">
+                <div className="input-group mb-3">
+                    <input onChange={this.filtrar} type="text" id="query" className="form-control" placeholder="Ingresa busqueda" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <div className="input-group-append">
+                      <button className="btn btn-success" type="submit" id="button-addon2">Buscar</button>
+                    </div>
+                </div>
+               </div>
+             </form>
             </div>
+          </div>
         </div>
+        <div className="Titulo">
+          <h2>Buddy's registrados</h2>
         </div>
+        <div className="container">
+          <div className="row">
+            {this.renderizarResultados()}
+          </div>
         </div>
-        </div>
-        </div>
-        <div className= "Titulo">
-        <h2>Buddy's registrados</h2>
-        </div>
-        <Persona/>
+        {/* <Persona/> */}
       </div>
     );
-     }
- }
-  
+  }
+}
+
 export default Buscar;
